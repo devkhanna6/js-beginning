4fef325b920f193da8926155a83241d4aeceafc5
devkhanna6 <devkhanna0607@gmail.com>
2025-04-05 12:53:52 +0000

    Basics od Day and Time

---
+++ .devcontainer/02_dates.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2025, 04, 05)
+// console.log(myCreatedDate.toDateString)
+
+let myTimeStamp = Date.now
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime)
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth)
