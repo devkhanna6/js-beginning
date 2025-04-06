96cf39039dab8a143feb4f4026c23600d1473087
devkhanna6 <devkhanna0607@gmail.com>
2025-04-06 09:23:36 +0000

    Basics of Array

---
+++ .devcontainer/01_basics/03_array.js
@@ -0,0 +1,12 @@
+const marvel_heros = ["Thor","Spiderrman","Hulk"]
+const dc_heros = ["Flash","Batman","Superman"]
+
+console.log(Array.isArray("Dev"))
+console.log(Array.from("Dev"))
+console.log(Array.from({name: "Dev"} ))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
