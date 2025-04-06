09e166a21a9519bbe58f56279f7799c8d061b0bd
devkhanna6 <devkhanna0607@gmail.com>
2025-04-06 18:49:45 +0000

    Functions basics

---
+++ .devcontainer/01_basics/05_functions.js
@@ -0,0 +1,19 @@
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// addTwoNumbers(3, 4)
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2
+    return result
+}
+
+// console.log(addTwoNumbers(3, 4))
+
+function calculateCartPrice(...num1){
+    return num1
+
+}
+
+console.log(calculateCartPrice(200, 300, 400))
